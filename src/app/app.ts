868d76f885248a55f4225f31f82c21fe0c4aba32
Luana Cardoso <luanamartinscardoso02@gmail.com>
2026-07-06 16:24:41 -0300

    feat(app.ts): Adiciona componente Produto ao app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+//!import { RouterOutlet } from '@angular/router';//Remove o uso do RouterOutlet por enquanto! Não está em uso.
+import { Produto } from './components/produto/produto'; //Importa o components do produto para ser exibido no app criado
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Produto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
