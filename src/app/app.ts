ff8da0852eaeff7cb782c4ec309de17b73383ebd
Luana Cardoso <luanamartinscardoso02@gamil.com>
2026-07-14 17:23:12 -0300

    feat: criou imports da lista no app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
-//!import { RouterOutlet } from '@angular/router';//Remove o uso do RouterOutlet por enquanto! Não está em uso.
-import { Produto } from './components/produto/produto'; //Importa o components do produto para ser exibido no app criado
+import { RouterOutlet } from '@angular/router'
+import { Produto } from './components/produto/produto'; 
+import {ListaProdutos} from './components/lista-produtos/lista-produtos'; 
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListaProdutos],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
 }
+//criação da lista de produtos e o import da mesma
