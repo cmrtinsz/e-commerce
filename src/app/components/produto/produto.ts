9b4b78b44ad07405779f0adb0a71115ae0e7ae08
Luana Cardoso <luanamartinscardoso02@gamil.com>
2026-07-14 14:54:36 -0300

    feat(produto.ts):cria lista de produtos e no import, mudamos o valor/preço para o R$

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
+import{UpperCasePipe, CurrencyPipe} from '@angular/common';
+import { PrecoFormatadoPipe } from '../../pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe, PrecoFormatadoPipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
 export class Produto {
-  produto = 'Notebook';
-  preco = 2500;
-  mostrarPreco = true
-  mostrarProduto = false;
+ // produto = 'Notebook';
+ // preco = 2500;
+ // mostrarPreco = true
+ // mostrarProduto = false;
+ produto = [
+  {produto:'Monitor',preco: 1000 } ,
+  {produto: 'Gabinete' ,preco: 2500} ,
+  {produto: 'Fone Gamer' , preco: 500} ,
+  {produto: 'Mouse' , preco: 1200} ,
+  {produto: 'Cadeira Gamer' , preco: 3000}
+ ]
 }
